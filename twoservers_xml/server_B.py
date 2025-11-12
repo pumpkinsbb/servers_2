6ab8be65f1e66fcee31ce1dfa803ebdc4a4c3321
pumpkinsbb <goldovanskaevgenia@gmail.com>
2025-11-12 21:44:51 +0200

    Add files via upload

--- twoservers_xml/server_B.py
+++ twoservers_xml/server_B.py
@@ -1,7 +1,16 @@
+import json
+
 from flask import Flask,jsonify, request, Response
+import redis
 import xmltodict
 
 app = Flask(__name__)
+def connect():
+    return redis.Redis(
+    host='localhost',
+    port=6379,
+    db=0
+)
 
 @app.route("/xml", methods = ["POST"])
 def json_to_xml():
@@ -9,6 +18,8 @@ def json_to_xml():
     json_data["person"]["name"]=json_data["person"]["name"]+f" "+json_data["person"]["surname"]
     del json_data["person"]["surname"]
     xml_data = xmltodict.unparse(json_data, pretty=True)
+    r = connect()
+    r.set("xml",xml_data )
     return Response(xml_data, mimetype="application/xml", status=200)
 
 @app.route("/json", methods = ["POST"])
@@ -17,7 +28,23 @@ def xml_to_json():
     json_data = xmltodict.parse(xml_data)
     json_data["person"]["name"] = json_data["person"]["name"] + f" " + json_data["person"]["surname"]
     del json_data["person"]["surname"]
+    r = connect()
+    r.set("json", json.dumps(json_data))
     return jsonify(json_data)
+
+@app.route("/getj", methods = ["GET"])
+def get_data():
+    key = request.args.get("key")
+    r = connect()
+    item = r.get(key)
+    return item.decode()
+
+@app.route("/del", methods = ["POST"])
+def del_data():
+    key = request.args.get("key")
+    r = connect()
+    r.delete(key)
+    return jsonify({"message":"opsie"})
 
 if __name__ == "__main__":
     app.run(port= 5001)
